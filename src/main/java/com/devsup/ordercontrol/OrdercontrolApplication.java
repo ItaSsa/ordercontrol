dc1e4e83bd5b21c7206185f4e410b2dd53e5606f
ItaSsa <itairassa@gmail.com>
2023-05-19 17:02:30 -0300

    Testing using code 1309

--- src/main/java/com/devsup/ordercontrol/OrdercontrolApplication.java
+++ src/main/java/com/devsup/ordercontrol/OrdercontrolApplication.java
@@ -20,8 +20,8 @@ public class OrdercontrolApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		//Order order = new Order(1034,150.00,20.00);
-		Order order = new Order(2282,800.00,10.00);
-
+		//Order order = new Order(2282,800.00,10.00);
+		Order order = new Order(1309,95.90,0.00);
 		System.out.println(orderService.total(order));
 	}
 }
